25cc1c220c76eb4db9a3c9fb1e712e48ea5b63b4
Cornelia Davis <cornelia@corneliadavis.com>
2023-02-20 13:22:06 -0800

    avoid use of count on the iterator
    Signed-off-by: Cornelia Davis <cornelia@corneliadavis.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+
+#![feature(iter_next_chunk)]
+
 use select::document::{Document, self};
 use select::node::{Data, Raw};
 use select::predicate::{Attr, Class, Name, Predicate};
@@ -24,18 +27,23 @@ fn main() {
     let all_elements = document.find(Class("outer-cell"));
     for node in all_elements {
 
-        // entries with three anchors are the "real" ones - video, channel and link to settings.
-        if node.find(Name("a")).count() == 3 {
+        let mut all_anchors = node.find(Name("a"));
 
-            let mut anchors = node.find(Name("a"));
-            let video_a = anchors.next().unwrap(); 
+        // entries with three anchors are the "real" ones - video, channel and link to settings.
+        let three_anchors = all_anchors.next_chunk::<3>();
+
+        if three_anchors.is_ok() {
+
+            let anchors = three_anchors.ok().unwrap();
+            let video_a = anchors[0];
             let video_url = video_a.attr("href").unwrap();
             let video_title = video_a.first_child().unwrap().as_text().unwrap();
-            let channel_a = anchors.next().unwrap();
+            let channel_a = anchors[1];
             let channel_url = channel_a.attr("href").unwrap();
             let channel_title = channel_a.first_child().unwrap().as_text().unwrap();
             let date_str = channel_a.parent().unwrap().last_child().unwrap().text();
             let date_watched = NaiveDateTime::parse_from_str(date_str.as_str().trim(), "%b %e, %Y, %_I:%M:%S %p %Z").expect("Error parsing datetime");
+
             my_videos.push(Video {
                 video_url,
                 video_title,
@@ -44,9 +52,10 @@ fn main() {
                 date_str,
                 date_watched,
             });
+
         }
     }
-
+     
     for i in 0..10 {
         println!("{:?}", my_videos[i]);
     }
