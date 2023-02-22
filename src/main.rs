ea62d20fdc7fce70c32820664d075b1e2ff303b7
Cornelia Davis <cornelia@corneliadavis.com>
2023-02-21 17:35:02 -0800

    Switch from 'select' html scraping  module to the 'scraper' html scraping
    Signed-off-by: Cornelia Davis <cornelia@corneliadavis.com>

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,79 @@
-
 #![feature(iter_next_chunk)]
-#![feature(option_result_contains)]
 
-use select::document::{Document, self};
-use select::node::{Data, Raw, Node};
-use select::predicate::{Attr, Class, Name, Predicate, Descendant, And};
-use chrono::{DateTime, NaiveDateTime, FixedOffset};
-use std::iter::OnceWith;
-use std::mem;
+use chrono::NaiveDateTime;
+use scraper::{Html, Selector};
+use std::fs;
 
 #[derive(Debug)]
-pub struct Video<'a> {
-    pub video_url : &'a str,
-    pub video_title : &'a str,
-    pub channel_url : &'a str,
-    pub channel_title : &'a str,
-    pub date_str : String,
-    pub date_watched : NaiveDateTime,
+pub struct Video {
+    pub video_url: String,
+    pub video_title: String,
+    pub channel_url: String,
+    pub channel_title: String,
+    pub date_watched: NaiveDateTime,
 }
 
 fn main() {
-
     let mut my_videos: Vec<Video> = vec![];
 
-    let document = Document::from(include_str!("../watch-history.html"));
-    println!("loaded doc - num nodes {}", document.nodes.len());
+    let document = Html::parse_document(fs::read_to_string("watch-history.html").unwrap().as_str());
 
-    // The following finds divs with class outer-cell (this is an entry in the list that represents a video) AND
-    // does not contain the string "Google Ad" in the descendants - that is, it filters out any videos that were 
-    // ads. In the end, I used different logic because this is only one way that ads show up in the feed. What I
-    // am using now is the existence of 3 anchors within that div.
-    // Keeping the code in here though, because it took me some doing to figure it out. :-) 
-    // let all_elements 
-    //     = document.find(And(Class("outer-cell"), |node: &Node| !node.inner_html().contains(&"Google Ad")));
+    let all_elements_selector = Selector::parse("div.outer-cell").unwrap();
+    let all_elements = document.select(&all_elements_selector);
+    // println!("loaded doc - num nodes {}", all_elements.count());  //scraper::Html is an iterator so cannot consume it here and use it later.
 
-    let all_elements = document.find(Class("outer-cell"));
+    // Okay, so I am finding the selector use here at least as clunky as that of select. I do think the more complex 
+    // query will be cleaner.
+    // Update: I actually don't think so. It looks like CSS does not have such a selector.
+    // let all_elements_selector = Selector::parse("div.outer-cell <and what I want here is a way to say the inner_html contains a certain string>").unwrap();
+    // let all_elements = document.select(&all_elements_selector);
+
     for node in all_elements {
-
-        let mut all_anchors = node.find(Name("a"));
-
-        // entries with three anchors are the "real" ones - video, channel and link to settings.
-        let three_anchors = all_anchors.next_chunk::<3>();
+        let three_anchors = node.select(&Selector::parse("a").unwrap()).next_chunk::<3>();
 
         if three_anchors.is_ok() {
-
             let anchors = three_anchors.ok().unwrap();
             let video_a = anchors[0];
-            let video_url = video_a.attr("href").unwrap();
-            let video_title = video_a.first_child().unwrap().as_text().unwrap();
+            let video_url = video_a.value().attr("href").unwrap().to_string();
+            let video_title = video_a.inner_html().to_string();
             let channel_a = anchors[1];
-            let channel_url = channel_a.attr("href").unwrap();
-            let channel_title = channel_a.first_child().unwrap().as_text().unwrap();
-            let date_str = channel_a.parent().unwrap().last_child().unwrap().text();
-            let date_watched = NaiveDateTime::parse_from_str(date_str.as_str().trim(), "%b %e, %Y, %_I:%M:%S %p %Z").expect("Error parsing datetime");
+            let channel_url = channel_a.value().attr("href").unwrap().to_string();
+            let channel_title = channel_a.inner_html().to_string();
+            let date_str = channel_a.parent().unwrap().last_child().unwrap().value().as_text().unwrap().to_string();
+            let date_watched = NaiveDateTime::parse_from_str(
+                date_str.as_str().trim(),
+                "%b %e, %Y, %_I:%M:%S %p %Z",
+            )
+            .expect("Error parsing datetime");
 
             my_videos.push(Video {
                 video_url,
                 video_title,
                 channel_url,
                 channel_title,
-                date_str,
                 date_watched,
             });
-
         }
     }
-     
+
     for i in 0..10 {
         println!("{:?}", my_videos[i]);
     }
 
-    fn print_subtree(document: &Document, dep: usize, index: usize) {
-        for i in 0..dep {
-            print!("  ");
-        }
-        println!("{:p} {:?}", &(document.nodes[index]), document.nodes[index]);
-        match document.nodes[index].data {
-            Data::Element (_, _) => {
-                let mut childi = document.nodes[index].first_child;
-                while childi.is_some() {
-                    print_subtree(document, dep+1, childi.unwrap());
-                    childi = document.nodes[childi.unwrap()].next;
-                }
-            }
-            _ => (),
-        }
-    }
+    // fn print_subtree(document: &Document, dep: usize, index: usize) {
+    //     for i in 0..dep {
+    //         print!("  ");
+    //     }
+    //     println!("{:p} {:?}", &(document.nodes[index]), document.nodes[index]);
+    //     match document.nodes[index].data {
+    //         Data::Element (_, _) => {
+    //             let mut childi = document.nodes[index].first_child;
+    //             while childi.is_some() {
+    //                 print_subtree(document, dep+1, childi.unwrap());
+    //                 childi = document.nodes[childi.unwrap()].next;
+    //             }
+    //         }
+    //         _ => (),
+    //     }
+    // }
 }
