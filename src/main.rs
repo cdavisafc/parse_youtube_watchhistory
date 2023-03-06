b61ac285ed27390bca9d1534726be5d57a1d37e1
Cornelia Davis <cornelia@corneliadavis.com>
2023-03-06 15:52:20 -0800

    This once again works. Going to see if I can break it by changing things in the main.
    Signed-off-by: Cornelia Davis <cornelia@corneliadavis.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(option_result_contains)]
 #![feature(iter_next_chunk)]
 
+use select::document::Document;
+use crate::youtube::Video;
+
 pub mod utils;
 pub mod youtube;
 
@@ -8,13 +11,14 @@ pub mod youtube;
 const FILENAME : &str = "../watch-history.html";
 fn main() {
 
-    let (document, my_videos) = youtube::parse_youtube_takeout();
+    let document = Document::from(include_str!("../watch-history.html"));
+    let my_videos = youtube::parse_youtube_takeout(&document);
 
     println!("{}", document.nodes.len());
 
-    // for i in 0..10 {
-    //     println!("{:?}", my_videos[i]);
-    // }
+    for i in 0..10 {
+        println!("{:?}", my_videos[i]);
+    }
 
 //    println!("{my_videos}");
 
--- src/youtube.rs
+++ src/youtube.rs
@@ -19,11 +19,10 @@ pub struct Video<'a> {
     pub date_watched: NaiveDateTime,
 }
 
-pub fn parse_youtube_takeout<'a>() -> (Document, Vec<Video<'a>>) {
+pub fn parse_youtube_takeout<'a>(document: &'a Document) -> Vec<Video<'a>> {
     let mut my_videos = Vec::new();
-    //    let s = String::from("foobar");
 
-    let document = Document::from(include_str!("../watch-history.html"));
+    //let document = Document::from(include_str!("../watch-history.html"));
     println!("loaded doc - num nodes {}", document.nodes.len());
 
     // The following finds divs with class outer-cell (this is an entry in the list that represents a video) AND
@@ -34,46 +33,44 @@ pub fn parse_youtube_takeout<'a>() -> (Document, Vec<Video<'a>>) {
     // let all_elements
     //     = document.find(And(Class("outer-cell"), |node: &Node| !node.inner_html().contains(&"Google Ad")));
 
-    {
-        let all_elements = document.find(Class("outer-cell"));
-        for node in all_elements {
-            //       utils::print_subtree(&document, 0, node.index());
+    let all_elements = document.find(Class("outer-cell"));
+    for node in all_elements {
+        //       utils::print_subtree(&document, 0, node.index());
 
-            let mut all_anchors = node.find(Name("a"));
+        let mut all_anchors = node.find(Name("a"));
 
-            // entries with three anchors are the "real" ones - video, channel and link to settings.
-            let three_anchors = all_anchors.next_chunk::<3>();
+        // entries with three anchors are the "real" ones - video, channel and link to settings.
+        let three_anchors = all_anchors.next_chunk::<3>();
 
-            if three_anchors.is_ok() {
-                let anchors = three_anchors.ok().unwrap();
-                let video_a = anchors[0];
-                let video_url = video_a.attr("href").unwrap();
-                let video_title = video_a.first_child().unwrap().as_text().unwrap();
-                let channel_a = anchors[1];
-                let channel_url = channel_a.attr("href").unwrap();
-                let channel_title = channel_a.first_child().unwrap().as_text().unwrap();
-                let date_str = channel_a.parent().unwrap().last_child().unwrap().text();
-                let date_watched = NaiveDateTime::parse_from_str(
-                    date_str.as_str().trim(),
-                    "%b %e, %Y, %_I:%M:%S %p %Z",
-                )
-                .expect("Error parsing datetime");
+        if three_anchors.is_ok() {
+            let anchors = three_anchors.ok().unwrap();
+            let video_a = anchors[0];
+            let video_url = video_a.attr("href").unwrap();
+            let video_title = video_a.first_child().unwrap().as_text().unwrap();
+            let channel_a = anchors[1];
+            let channel_url = channel_a.attr("href").unwrap();
+            let channel_title = channel_a.first_child().unwrap().as_text().unwrap();
+            let date_str = channel_a.parent().unwrap().last_child().unwrap().text();
+            let date_watched = NaiveDateTime::parse_from_str(
+                date_str.as_str().trim(),
+                "%b %e, %Y, %_I:%M:%S %p %Z",
+            )
+            .expect("Error parsing datetime");
 
-                my_videos.push(Video { 
-                    video_url : "a",
-                    video_title : "a",
-                    channel_url : "a",
-                    channel_title : "a",
-                    date_watched,
-                });
-            }
+            my_videos.push(Video {
+                video_url,
+                video_title,
+                channel_url,
+                channel_title,
+                date_watched,
+            });
         }
-
-        println!("{}", document.nodes.len());
-
-    for i in 0..10 {
-        println!("{:?}", my_videos[i]);
     }
-    }
-    (document, my_videos)
+
+    my_videos
+    // println!("{}", document.nodes.len());
+
+    // for i in 0..10 {
+    //     println!("{:?}", my_videos[i]);
+    // }
 }
